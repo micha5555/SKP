10f44dc2a6903bb716c32bf4167ba452f4418676
Maja Nagarnowicz <72594569+nebraszka@users.noreply.github.com>
2023-01-24 11:40:36 +0100

    Add structure of mobile app

---
+++ MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
@@ -0,0 +1,11 @@
+package pw.ee.proj_zesp.skp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
