62d3cc6c4d9b695a66ea8b074f2c810a02cca230
Michał Ziober <michalziober08@gmail.com>
2023-03-04 11:45:48 +0100

    gui[MOBI_SKP-50] create xml files with views from figma

--- MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
+++ MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
@@ -1,50 +1,11 @@
 package pw.ee.proj_zesp.skp
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import pw.ee.proj_zesp.skp.kontrolastart.KontrolaStart
-import pw.ee.proj_zesp.skp.kontrolastop.KontrolaStop
-import pw.ee.proj_zesp.skp.menu.Menu
-import pw.ee.proj_zesp.skp.osystemie.OSystemie
-import pw.ee.proj_zesp.skp.ui.theme.SKPTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SKPTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-//                    Menu()
-                    KontrolaStart()
-//                    KontrolaStop()
-//                    OSystemie();
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SKPTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
