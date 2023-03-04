662edbf0b9693c74c47169a7f9e068a28cbcddc3
Jakub Wyłucki <jakub.wylucki@gmail.com>
2023-03-04 12:02:29 +0100

    fix-WEB&API-128-repairing_connection_to_db

--- rest/app/__init__.py
+++ rest/app/__init__.py
@@ -1,17 +1,14 @@
 from flask import Flask
 from config import Config
-# from app.db import db
-# import json
+from app.db import db
 
 def create_app(config = Config):
     app = Flask(__name__)
     app.config.from_object(config)
-    # app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:root@172.23.23.23:3306/skp_test'
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # db.init_app(app)
+    db.init_app(app)
 
-    # @app.route("/test", methods=['GET'])
-    # def test():
-    #     return json.dumps("test")
+    @app.route("/", methods=['GET'])
+    def test():
+        return "test"
     
     return app
--- rest/app/models/reportModel.py
+++ rest/app/models/reportModel.py
@@ -4,6 +4,8 @@ class Report(db.Model):
     __tablename__ = 'report'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, server_default=db.func.now())
+    start_period = db.Column(db.DateTime)
+    stop_period = db.Column(db.DateTime)
     filename = db.Column(db.String(120), unique=False, nullable=False)
     description = db.Column(db.Text, unique=False, nullable=False) # description from when to when 
     
