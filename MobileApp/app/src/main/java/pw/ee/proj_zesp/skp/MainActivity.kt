f8f2dc7c11bc894f486f1cf77e9dcef133a9c05f
Jakub Maciejewski <jakubm55555@gmail.com>
2023-03-13 19:53:50 +0100

    [50]: Configuration fix
    Change gradle version to be compatible with project

--- MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
+++ MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.about_app)
+        setContentView(R.layout.main_menu)
 
     }
 }
