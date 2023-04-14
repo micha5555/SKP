e1f97a1028ae2f9b2223541e8f2af191446d50b7
Michał Ziober <73911673+micha5555@users.noreply.github.com>
2023-04-15 00:41:24 +0200

    Mobile.getting accesses (#6)
    * added localisation; functions for debugging
    
    * [SKP-166]:get_localisation_access_and_create_function_to_get_current_localisation

--- MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
+++ MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
@@ -1,14 +1,20 @@
 package pw.ee.proj_zesp.skp
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageButton
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatDelegate
+import java.io.*
 
 class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         getSupportActionBar()?.hide()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
