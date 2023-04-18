2877675e1b882c3055e38754ab74c55cd5bb97bc
Agnieszka Stefankowska <107104083+NeferHikari@users.noreply.github.com>
2023-04-18 21:09:27 +0200

    [173] Add more tests

--- rest/tests/test_notPaidCase.py
+++ rest/tests/test_notPaidCase.py
@@ -2,7 +2,6 @@ import unittest
 import copy
 from app import create_app
 
-#po dopisaniu walidatorów dopisać testy 
 class notPaidCase(unittest.TestCase):
     def setUp(self):
         app = create_app()
@@ -36,6 +35,21 @@ class notPaidCase(unittest.TestCase):
         response = self.client.post('/notPaidCase/add', json=caseData)
         self.assertGreaterEqual(response.status_code, 200)
         self.assertLess(response.status_code, 202)
+
+    def test_requestShouldNotAddCaseWhenNotValidateData(self):
+        caseData = {
+        "register_plate":"WA92829",
+        "datetime" :"2023-01-12T19:12:30Z",
+        "location":"-19.912086,-52.897761" ,
+        "image" :"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAMUlEQVR4nGIp5n3CgA1MYP+JVZwJqygeMKqBGMDI6ZGCVaL9lCF1bBjVQAwABAAA//9W/wVNpn8uyAAAAABJRU5ErkJggg==",
+        "probability":" 99.7", 
+        "controller_id":"1"  
+        }
+        for x in caseData:
+            newData=copy.deepcopy(caseData)
+            newData[x]="veryLongRandomStringThatShouldWork"
+            response = self.client.post('/notPaidCase/add', json=newData)
+            self.assertEqual(response.status_code, 406)
 
 if __name__ == '__main__':
     unittest.main()
--- rest/tests/test_problematicCase.py
+++ rest/tests/test_problematicCase.py
@@ -13,6 +13,9 @@ class ProblematicCaseTest(unittest.TestCase):
     def test_requestGetFirst(self):
         response = self.client.get('/problematicCase/1')
         self.assertEqual(response.status_code, 200)
+    def test_requestGetNotExist(self):
+        response = self.client.get('/problematicCase/10000')
+        self.assertEqual(response.status_code, 200)
     
     def test_requestAdd(self):
         caseData = {
@@ -26,7 +29,38 @@ class ProblematicCaseTest(unittest.TestCase):
         response = self.client.post('/problematicCase/add',json=caseData)
         self.assertEqual(response.status_code, 200)
 
-    def test_requestAdd(self):
+    def test_requestShouldNotAddCaseWhenNotValidateData(self):
+        caseData = {
+        "register_plate":"WA92829",
+        "datetime" :"2023-01-12T19:12:30Z",
+        "location":"-19.912086,-52.897761" ,
+        "image" :"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAMUlEQVR4nGIp5n3CgA1MYP+JVZwJqygeMKqBGMDI6ZGCVaL9lCF1bBjVQAwABAAA//9W/wVNpn8uyAAAAABJRU5ErkJggg==",
+        "probability":" 99.7", 
+        "controller_id":"1"  
+        }
+        for x in caseData:
+            newData=copy.deepcopy(caseData)
+            newData[x]="veryLongRandomStringThatShouldWork"
+            response = self.client.post('/problematicCase/add', json=newData)
+            self.assertEqual(response.status_code, 406)
+
+    def test_requestShouldNotAddCaseWithoutNecessaryElemensts(self):
+        caseData = {
+        "register_plate":"WA92829",
+        "datetime" :"2023-01-12T19:12:30Z",
+        "location":"-19.912086,-52.897761" ,
+        "image" :"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAMUlEQVR4nGIp5n3CgA1MYP+JVZwJqygeMKqBGMDI6ZGCVaL9lCF1bBjVQAwABAAA//9W/wVNpn8uyAAAAABJRU5ErkJggg==",
+        "probability":" 99.7", 
+        "controller_id":"1"  
+        }
+        for x in caseData:
+            newData=copy.deepcopy(caseData)
+            del newData[x]
+            response = self.client.post('/problematicCase/add', json=newData)
+            self.assertEqual(response.status_code, 400)
+            self.assertEqual(response.get_json(),{"error": "request is missing"})
+
+    def test_requestEdit(self):
         caseData = {
         "id":"1",
         "registration":"WA92829",
@@ -34,6 +68,31 @@ class ProblematicCaseTest(unittest.TestCase):
         }
         response = self.client.put('/problematicCase/edit',json=caseData)
         self.assertEqual(response.status_code, 200)
+
+    def test_requestEdit(self):
+        caseData = {
+        "id":"1",
+        "registration":"WA92829",
+        "administration_edit_time" :"2023-01-12T19:12:30Z",
+        }
+        for x in caseData:
+            newData=copy.deepcopy(caseData)
+            del newData[x]
+            response = self.client.post('/problematicCase/add', json=newData)
+            self.assertEqual(response.status_code, 400)
+            self.assertEqual(response.get_json(),{"error": "request is missing"})
+
+    def test_requestEdit(self):
+        caseData = {
+        "id":"1",
+        "registration":"WA92829",
+        "administration_edit_time" :"2023-01-12T19:12:30Z",
+        }
+        for x in caseData:
+            newData=copy.deepcopy(caseData)
+            newData[x]="veryLongRandomStringThatShouldWork"
+            response = self.client.post('/problematicCase/add', json=newData)
+            self.assertEqual(response.status_code, 406)
 
     def test_requestShouldCorrectionCase(self):
         caseData = {
@@ -44,6 +103,31 @@ class ProblematicCaseTest(unittest.TestCase):
         response = self.client.post('/problematicCase/correction', json=caseData)
         self.assertGreaterEqual(response.status_code, 200)
         self.assertLess(response.status_code, 202)
+
+    def test_requestShouldCorrectionCase(self):
+        caseData = {
+        "id":"1",
+        "status":"not_possible_to_check",
+        "admin_id":"1"  
+        }
+        for x in caseData:
+            newData=copy.deepcopy(caseData)
+            newData[x]="veryLongRandomStringThatShouldWork"
+            response = self.client.post('/problematicCase/correction', json=newData)
+            self.assertEqual(response.status_code, 406)
+
+    def test_requestShouldCorrectionCase(self):
+        caseData = {
+        "id":"1",
+        "status":"not_possible_to_check",
+        "admin_id":"1"  
+        }
+        for x in caseData:
+            newData=copy.deepcopy(caseData)
+            del newData[x]
+            response = self.client.post('/problematicCase/correction', json=newData)
+            self.assertEqual(response.status_code, 406)
+
 
 if __name__ == '__main__':
     unittest.main()
