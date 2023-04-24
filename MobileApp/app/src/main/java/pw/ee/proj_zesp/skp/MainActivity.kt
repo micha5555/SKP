9c1819ffbf6e905f3585e07e5c8690082ca05447
Michał Ziober <michalziober08@gmail.com>
2023-04-24 21:41:40 +0200

    [SKP-166]:fix_getting_accesses_to_location

--- MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
+++ MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
@@ -1,85 +1,58 @@
 package pw.ee.proj_zesp.skp
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.Intent
-
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.ImageButton
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.res.ResourcesCompat
-import java.util.*
-
 import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import pw.ee.proj_zesp.skp.utils.NavigationUtils
 import java.io.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
-
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
-
-//        var req: SKPRequest = SKPRequest("date and time", "location in the middle of nowhere", "WA54RT", "45.6")
-//        Log.i("dt", req.getDatetime().toString())
-//        Log.i("lt", req.getLocation().toString())
-//        Log.i("rp", req.getRegisterPlate().toString())
-//        Log.i("pb", req.getProbability().toString())
-//        req.send()
-//        Log.i("rsp", req.getResponse().toString())
-
-//        var drawable = ContextCompat.getDrawable(this, R.drawable.image)
-//        val drawable = BitmapFactory.decodeResource(resources, R.drawable.psiel)
-
-
-        val drawable: Drawable? =
-            ResourcesCompat.getDrawable(resources, R.drawable.psiel, null)
-
-        var bitmap: Bitmap = Bitmap.createBitmap(
-            drawable!!.intrinsicWidth,
-            drawable!!.intrinsicHeight,
-            Bitmap.Config.ARGB_8888
-        )
-        val canvas = Canvas(bitmap)
-        drawable.setBounds(0, 0, canvas.width, canvas.height)
-        drawable.draw(canvas)
-
-
-//        Log.i("imvie", "ggg")
-
+        requestLocation()
         getSupportActionBar()?.hide()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
 
-//        val req: SKPRequest = SKPRequest("some date", "some location", "some plate", "some probability", bitmap)
-//        val imv: ImageView= findViewById<ImageView>(R.id.ttt);
-//        req.send()
-//        req.sendGet()
-//        Log.i("resp", req.getResponse().toString())
-
-
-//        Log.i("drawbl", drawable.toString())
-//        val response = req.sendGetRequest("https://example.com/api/endpoint")
-//        Log.d("REST response", response)
-
-//        val restRequestTask = SKPRequest("https://stackoverflow.com/questions/46177133/http-request-in-android-with-kotlin")
-//        restRequestTask.execute()
-
-//        System.out.println(" C DATE is  "+currentDate)
-        val currentLocation = NavigationUtils.getLocation(this)
-//        val sr = SKPRequest()
-//        sr.send(currentLocation, "123", "123")
-
         val aboutAppButton: ImageButton = findViewById<ImageButton>(R.id.about_system_button)
-//        aboutAppButton.setImageBitmap(bitmap)
-
+        val location = NavigationUtils.getLocation(this)
+        Log.i("current localisation", location.toString())
         aboutAppButton.setOnClickListener {
             val intent = Intent(this, AboutAppActivity::class.java)
             this.startActivity(intent)
 
+        }
+    }
+    private fun requestLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                ACCESS_FINE_LOCATION) !==
+            PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION), 1)
+            } else {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION), 1)
+            }
         }
     }
 }
