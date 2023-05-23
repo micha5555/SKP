8f8fe7e71d93886471e1219d1f24a52b61ffa903
Jakub Wyłucki <jakub.wylucki@gmail.com>
2023-05-24 00:07:35 +0200

    [193] Add varables to gereate reports

--- rest/app/generators.py
+++ rest/app/generators.py
@@ -1,6 +1,8 @@
 import xlsxwriter
 from fpdf import FPDF
 from config import Config
+from app.models.notPaidCaseModel import NotPaidCase
+from app.models.problematicCaseModel import ProblematicCase
 import os
 
 def generateXLSX(filename,notPaidCases,problematicCases):
@@ -52,19 +54,19 @@ def generateXLSX(filename,notPaidCases,problematicCases):
         row += 1
     xlsx.close()
 
-def generatePDF(filename):
+def generatePDF(filename, start_period, end_peroid, notPaidCases, problematicCases):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font('Arial', '', 20)
-    txt = "SKP Report from: " +data['start_peroid'] +" to: "+data['end_peroid']
+    txt = "SKP Report from: " + start_period + " to: " + end_peroid
     pdf.cell(w=0, h=10, txt=txt, ln=1)
     pdf.set_font('Arial', '', 16)
     txt = "Total number of not paid cases: "+str(len(notPaidCases))
     pdf.cell(w=0, h=10, txt=txt, ln=1)
     txt = "Total number of problematic cases: "+str(len(problematicCases))
     pdf.cell(w=0, h=10, txt=txt, ln=1)
-    current_date = start_date
-    while current_date <= end_date:
+    current_date = start_period
+    while current_date <= end_peroid:
         txt = "Date: "+current_date.strftime("%d/%m/%Y")
         pdf.cell(w=0, h=10, txt=txt, ln=1)
         notPaidCasesForDay = NotPaidCase.query.filter(
@@ -75,8 +77,10 @@ def generatePDF(filename):
         NotPaidCase.detect_time == current_date).all()
         txt = "Number of problematic cases: "+str(len(problematicCases))
         pdf.cell(w=0, h=10, txt=txt, ln=1)
+        
     pdfDest=os.path.join(
         os.getcwd(), 
         Config.REPORT_FOLDER, 
         filename + '.pdf')
     pdf.output(pdfDest,'F')
+    
