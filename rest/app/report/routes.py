b5907cc8a535c5a47e9f9c8701d9ebaf553250ad
Jakub Wyłucki <jakub.wylucki@gmail.com>
2023-05-24 00:08:24 +0200

    [193] Changes in routes

--- rest/app/report/routes.py
+++ rest/app/report/routes.py
@@ -1,9 +1,9 @@
+from flask import request, send_file
 from app.report import bp
 from app.db import db
 from app.models.reportModel import Report
 from app.models.notPaidCaseModel import NotPaidCase
 from app.models.problematicCaseModel import ProblematicCase
-from flask import request, make_response, send_file
 from app.validators import *
 from app.extensions import *
 from datetime import datetime
@@ -16,58 +16,76 @@ def get():
     reports = Report.query.order_by(Report.date.asc()).all()
     report_json = []
     if reports is None:
-        return {"error": "No report in DB"}, 404
+        return "W bazie nie ma żadnych raportów", 404
     else:
         for report in reports:
             report_json.append(report.json())
         response_data = report_json
-        response = make_response(response_data)
-        response.headers['Content-Type'] = 'application/json'
-        return response, 200
+        return makeResponse(response_data, 200)
 
 
-@bp.route('/download/<id>')
+@bp.route('/download/pdf/<id>')
 def download(id):
     if not validateId(id):
-        return {"error": "Element does not exists"}, 404
+        return "Plik o danym id nie istnieje", 404
+    
     report = Report.query.filter_by(id=id).first()
     path=os.path.join(
-            os.getcwd(), 
-            Config.REPORT_FOLDER,report.file_name,"pdf")
-    return send_file(path, as_attachment=True) #poprawiłam ścieżke ale czy my to chcemy zipować? albo potrzebujemy 2 routów 
+        os.getcwd(), 
+        Config.REPORT_FOLDER,
+        report.file_name,
+        "pdf"
+    )
+    return send_file(path, as_attachment=True)
+
+@bp.route('/download/csv/<id>')
+def download(id):
+    if not validateId(id):
+        return "Plik o danym id nie istnieje", 404
+    
+    report = Report.query.filter_by(id=id).first()
+    path=os.path.join(
+        os.getcwd(), 
+        Config.REPORT_FOLDER,
+        report.file_name,
+        "csv"
+    )
+    return send_file(path, as_attachment=True)
 
 
 @bp.route('/add', methods=["POST"])
 def create():
-    if request.method == "POST":
-        data = getRequestData(request)
-        if data is None:
-            return {"error": "request is null"}, 404
+    data = getRequestData(request)
 
-        if not validateDate(data['start_peroid']) and not validateDate(data['end_peroid']):
-            return {"error": "dates not accepted"}, 404
-        
-        start_peroid = datetime.strptime(data['start_peroid'], '%Y-%m-%dT%H:%M:%SZ')
-        end_peroid = datetime.strptime(data['end_peroid'], '%Y-%m-%dT%H:%M:%SZ')
-        
-        notPaidCases = NotPaidCase.query.filter(
-            NotPaidCase.detect_time <= end_peroid, 
-            NotPaidCase.detect_time>= start_peroid
-        ).all()
-        
-        problematicCases = ProblematicCase.query.filter(
-            ProblematicCase.detect_time >= start_peroid, 
-            ProblematicCase.detect_time <= end_peroid
-        ).all()
+    if not allElementsInList(Report.attr, data):
+        return "Zapytanie nie zawiera wszystkich wymaganych wartości", 400
 
-        filename = start_peroid.strftime("%Y%m%d")+"-"+end_peroid.strftime("%Y%m%d")
-        description = data['start_peroid']+":"+data['end_peroid']
-        report = Report(filename, description)
-        
-        pdfFilename = filename+'.pdf'
-        generatePDF()
+    if not validateDate(data['start_period']) and not validateDate(data['end_peroid']):
+        return "Podane formaty dat nie są poprawne", 404
+    
+    start_period = datetime.strptime(data['start_period'], '%Y-%m-%dT%H:%M:%SZ')
+    end_peroid = datetime.strptime(data['end_peroid'], '%Y-%m-%dT%H:%M:%SZ')
+    
+    notPaidCases = NotPaidCase.query.filter(
+        NotPaidCase.detect_time <= end_peroid, 
+        NotPaidCase.detect_time>= start_period
+    ).all()
+    
+    problematicCases = ProblematicCase.query.filter(
+        ProblematicCase.detect_time >= start_period, 
+        ProblematicCase.detect_time <= end_peroid
+    ).all()
 
-        xlsxFilename = filename+'.xlsx'
-        generateXLSX(filename, notPaidCases, problematicCases)
+    filename = start_period.strftime("%Y%m%d")+"-"+end_peroid.strftime("%Y%m%d")
+    report = Report(start_period, end_peroid, filename)
+    db.session.add(report)
+    db.session.commit()
+
+    pdfFilename = filename + '_' + getUuid() + '.pdf'
+    generatePDF(pdfFilename, start_period, end_peroid, notPaidCases, problematicCases)
+
+    xlsxFilename = filename + '_' + getUuid() + '.xlsx'
+    generateXLSX(filename, notPaidCases, problematicCases)
         
-        return {"xslx_name": xlsxFilename, "pdf_name": pdfFilename}, 200
+    data = {"xslx_name": xlsxFilename, "pdf_name": pdfFilename}
+    return makeResponse(data, 200)
--- rest/app/user/routes.py
+++ rest/app/user/routes.py
@@ -1,136 +1,133 @@
-from flask import request,make_response,session
+from flask import request, session
 from app.user import bp
 from app.models.userModel import User
 from app.validators import *
 from app.extensions import *
 from app.db import db
-import json
 
 @bp.route('/login', methods=["POST"])
 def login():
-    if request.method == "POST":
-        data = getRequestData(request)
+    data = getRequestData(request)
 
-        if not allElementsInList(data, User.loginAttr):
-            return "Request dont have all elements", 400
-        if not validateLogin(data["login"]) or not validatePassword(data["password"]):
-            return "Login or password is not safe", 404
+    if not allElementsInList(data, User.loginAttr):
+        return "Request dont have all elements", 400
+    if not validateLogin(data["login"]) or not validatePassword(data["password"]):
+        return "Login or password is not safe", 404
 
-        login = data["login"]
-        password = data['password']
+    login = data["login"]
+    password = data['password']
 
-        user = User.query.filter_by(login=login).first()
-        if user is None:
-            return "User not found", 404
-        
-        if(checkPassword(password, user.password)):
-            session["id"]=user.id
-            payload = user.generatePayload()
-            auth_token = createToken(payload=payload, lifetime=60)
-            response_data = {'auth_token': auth_token[0], 'refresh_token': auth_token[1]}
-            response = make_response(response_data)
-            response.headers['Content-Type'] = 'application/json'
-            return response,200
-        else:
-            return "Incorrect password", 401
+    user = User.query.filter_by(login=login).first()
+    if user is None:
+        return "User not found", 404
+    
+    if(checkPassword(password, user.password)):
+        session["id"]=user.id
+        payload = user.generatePayload()
+        auth_token = createToken(payload=payload, lifetime=60)
+        response_data = {'auth_token': auth_token[0], 'refresh_token': auth_token[1]}
+        return makeResponse(response_data, 200)
+    return "Incorrect password", 401
 
 @bp.route('/user', methods=["GET"])
 def getAll():
-    if request.method == "GET":
-        users = User.query.all()
-        users_json=[]
-        if users is None:
-            return {"error":"No user in DB"},404
-        else:
-            for user in users:
-                users_json.append(user.json())
-            response_data =users_json
-            response = make_response(response_data)
-            response.headers['Content-Type'] = 'application/json'
-            return response,200 
+    users = User.query.all()
+    users_json=[]
+
+    if users is None:
+        return "W bazie nie ma użytkowników", 404
+    
+    for user in users:
+        users_json.append(user.json())
+    response_data =users_json
+    return makeResponse(response_data, 200)
 
 @bp.route('/user/<id>', methods=["GET"])
 def get(id):
-    if request.method == "GET":
-        if not validateId(id):
-            return {"error":"Id must be instance od integer"}, 404
+    if not validateId(id):
+        return "Podane id nie jest wartością numeryczną", 404
 
-        user = User.query.filter_by(id=id).first()
-        if user is None:
-            return {"error":"User not found"}, 404
-        else:
-            response_data = user.json()
-            response = make_response(response_data)
-            response.headers['Content-Type'] = 'application/json'
-            return response, 200
+    user = User.query.filter_by(id=id).first()
+    if user is None:
+        return "Użytkownik o podanym id nie istnieje", 404
+
+    response_data = user.json()
+    return makeResponse(response_data, 200)
 
 @bp.route('/user/add', methods=["POST"])
 def create():
-    if request.method == "POST":
-        data = getRequestData(request)
-        
-        if not allElementsInList(data, User.attr):
-            return {"error":"Request dont have all elements"}, 400
-        if not validateLogin(data["login"]) or not validatePassword(data["password"]):
-            return {"error":"Login or password is not safe"}, 404
-        if not validateName(data["first_name"]) or not validateName(data["last_name"]):
-            return {"error":"Firstname or surname is not correct"}, 404
-        if not validateBoolean(data["is_admin"]) or not validateBoolean(data["is_controller"]):
-            return {"error":"Boolean is not correct"}, 404
-        
-        checkUserExistence = User.query.filter_by(login=data['login']).first()
-        if checkUserExistence is not None:
-            return {"error":"Login already exist"}, 409
-        
-        user = User(
-            first_name = data["first_name"],
-            last_name = data["last_name"],
-            login = data["login"],
-            password = data["password"],
-            # Czy walidacja do boolean nie powinna być w wyżej a nie tu?        
-            is_admin = toBoolean(data["is_admin"]),
-            is_controller = toBoolean(data["is_controller"]),
-        )
-        
-        db.session.add(user)
-        db.session.commit()
-        return {'message': "User created successfully"}, 200
+    data = getRequestData(request)
+    
+    if not allElementsInList(data, User.attr):
+        return "Podane id nie jest wartością numeryczną", 404
+    if not validateLogin(data["login"]):
+        return "Podany login jest zbyt słaby", 404 
+    if not validatePassword(data["password"]):
+        return "Podane hasło jest za słabe", 404
+    if not validateName(data["first_name"]):
+        return "Imię zawiera niedozwolone znaki", 404
+    if not validateName(data["last_name"]):
+        return "Nazwisko zawiera niedozwolone znaki", 404
+    if not validateBoolean(data["is_admin"]) or not validateBoolean(data["is_controller"]):
+        return "Admin i kotroler przyjmują wartość true/false", 404
+    
+    checkUserExistence = User.query.filter_by(login=data['login']).first()
+    if checkUserExistence is not None:
+        return "Podany login jest już zajęty", 409
+    
+    user = User(
+        first_name = data["first_name"],
+        last_name = data["last_name"],
+        login = data["login"],
+        password = data["password"],
+        is_admin = toBoolean(data["is_admin"]),
+        is_controller = toBoolean(data["is_controller"]),
+    )
+    
+    db.session.add(user)
+    db.session.commit()
+    data = {'message': "Użytkownik został utworzony pomyślnie."}
+    return makeResponse(data, 202)
 
 @bp.route('/user/edit/<id>', methods=["PUT"])
 def edit(id):
-    if request.method == "PUT":
-        data = getRequestData(request)
-
-        if not validateId(id):
-            return {"error":"Id must be instance od integer"}, 404
-        if not allElementsInList(data, User.attr_edit):
-            return {"error":"Request don't have all elements"}, 400
-        if not validateName(data["first_name"]) or not validateName(data["last_name"]):
-            return {"error":"Firstname or surname is not correct"}, 404
-        if not validateLogin(data['login']):
-            return {"error":"Login is incorrect"}, 404
-        if not validateBoolean(data["is_admin"]) or not validateBoolean(data["is_controller"]):
-            return {"error":"Boolean is not correct"}, 404
-        
-        user = User.query.filter_by(id=id).first()
-        user.first_name = data["first_name"]
-        user.last_name = data["last_name"]
-        user.is_admin = toBoolean(data["is_admin"])
-        user.is_controller = toBoolean(data["is_controller"])
-        db.session.commit()
-        return {"message":"User changed successfully"}, 200
+    data = getRequestData(request)
+    
+    if not validateId(id):
+        return "Podane id nie jest wartością numeryczną", 404
+    if not allElementsInList(data, User.attr):
+        return "Podane id nie jest wartością numeryczną", 404
+    if not validateLogin(data["login"]):
+        return "Podany login jest zbyt słaby", 404 
+    if not validatePassword(data["password"]):
+        return "Podane hasło jest za słabe", 404
+    if not validateName(data["first_name"]):
+        return "Imię zawiera niedozwolone znaki", 404
+    if not validateName(data["last_name"]):
+        return "Nazwisko zawiera niedozwolone znaki", 404
+    if not validateBoolean(data["is_admin"]) or not validateBoolean(data["is_controller"]):
+        return "Admin i kotroler przyjmują wartość true/false", 404
+    
+    user = User.query.filter_by(id=id).first()
+    user.first_name = data["first_name"]
+    user.last_name = data["last_name"]
+    user.is_admin = toBoolean(data["is_admin"])
+    user.is_controller = toBoolean(data["is_controller"])
+    db.session.commit()
+    data = {"message":"Użytkownik został zapisany."}
+    return makeResponse(data, 201)
 
 
 @bp.route('/user/del/<id>', methods=["DELETE"])
 def delete(id):
-    if request.method == "DELETE":
-        if not validateId(id):
-            return {"error":"Id must be instance od integer"}, 404
-        
-        user = User.query.filter_by(id=id).first()
-        if user is None:
-            return {"error":"User not found"}, 404
-        else:
-            db.session.delete(user)
-            db.session.commit()
-            return {"message":"User removed"}, 200
+    if not validateId(id):
+        return "Podane id nie jest wartością numeryczną", 404
+    
+    user = User.query.filter_by(id=id).first()
+    if user is None:
+        return "Użytkownik o podanym id nie istnieje", 404
+    else:
+        db.session.delete(user)
+        db.session.commit()
+        data = {"message":"Użytkownik został usunięty."}
+        return makeResponse(data, 200)
