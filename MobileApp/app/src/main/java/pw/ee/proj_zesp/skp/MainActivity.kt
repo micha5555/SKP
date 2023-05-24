8293787a0a7cc0b6326af779a242fd7ed2932eae
Michał Ziober <73911673+micha5555@users.noreply.github.com>
2023-05-24 21:04:52 +0200

    Mobile.saving failed requests (#17)
    * made get request, still crawling
    
    * in progress
    
    * in progess
    
    * in progress
    
    * [SKP-166]:fix_getting_accesses_to_location
    
    * in progress
    
    * in progress
    
    * tmp commit; some changes will be discarded
    
    * [SKP-182]:add FailedRequest class; changes in SKPRequest
    
    * [SKP-182]:add method for sending failed requests in background
    
    * refactor SKPRequest; requests working
    
    * refactor

--- MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
+++ MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/MainActivity.kt
@@ -3,17 +3,23 @@ package pw.ee.proj_zesp.skp
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import pw.ee.proj_zesp.skp.utils.NavigationUtils
 import androidx.core.content.ContextCompat
+
+import pw.ee.proj_zesp.skp.utils.NavigationUtils
+
 import java.io.*
+import java.util.*
+
+import kotlin.concurrent.schedule
+
 
 class MainActivity : AppCompatActivity() {
-
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         NavigationUtils.requestLocationPermissions(this)
@@ -23,14 +29,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_menu)
 
         val aboutAppButton: ImageButton = findViewById<ImageButton>(R.id.about_system_button)
+
         val startControlButton: ImageButton = findViewById<ImageButton>(R.id.start_control_button_on_menuview)
         val button: Drawable = ContextCompat.getDrawable(this, R.drawable.button_shape)!!
         aboutAppButton.setBackgroundDrawable(button)
         startControlButton.setBackgroundDrawable(button)
 
+
         aboutAppButton.setOnClickListener {
             val intent = Intent(this, AboutAppActivity::class.java)
             this.startActivity(intent)
         }
+
+        val doge: Drawable = ContextCompat.getDrawable(this, R.drawable.doge)!!
+
+        val srequest = SKPRequest(false, doge, "90.111111,-160.222222", "098.20", "BZ4567", "2023-05-11T21:10:10Z")
+//        srequest.send(NavigationUtils.getLocation(this), )
+        srequest.send()
     }
 }
