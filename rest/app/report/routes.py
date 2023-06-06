469bf7024327ee6262ccc12d4484cad0fea55b29
Jakub Wyłucki <jakub.wylucki@gmail.com>
2023-06-06 19:00:58 +0200

    [204] Add raports download and view

--- rest/app/report/routes.py
+++ rest/app/report/routes.py
@@ -1,4 +1,4 @@
-from flask import request, send_file
+from flask import request, send_file, send_from_directory
 from app.report import bp
 from app.db import db
 from app.models.reportModel import Report
@@ -10,6 +10,7 @@ from app.auth import tokenAdminRequire
 from datetime import datetime
 from app.generators import *
 from config import Config
+import zipfile
 
 
 @bp.route('/', methods=["GET"])
@@ -29,33 +30,33 @@ def get(curr_user):
 @bp.route('/download/pdf/<id>')
 @tokenAdminRequire
 def download_pdf(curr_user, id):
+
     if not validateId(id):
         return "Plik o danym id nie istnieje", 404
     
     report = Report.query.filter_by(id=id).first()
-    path=os.path.join(
+    path_pdf=os.path.join(
         os.getcwd(), 
         Config.REPORT_FOLDER,
-        report.file_name,
-        "pdf"
+        report.filename + ".pdf"
     )
-    return send_file(path, as_attachment=True)
+    return send_file(path_pdf, as_attachment=True)
 
-@bp.route('/download/csv/<id>')
+
+@bp.route('/download/xlsx/<id>')
 @tokenAdminRequire
-def download_csv(curr_user, id):
+def download_xlsx(curr_user, id):
+
     if not validateId(id):
         return "Plik o danym id nie istnieje", 404
     
     report = Report.query.filter_by(id=id).first()
-    path=os.path.join(
+    path_xlsx=os.path.join(
         os.getcwd(), 
         Config.REPORT_FOLDER,
-        report.file_name,
-        "csv"
+        report.filename + ".xlsx"
     )
-    return send_file(path, as_attachment=True)
-
+    return send_file(path_xlsx, as_attachment=True)
 
 @bp.route('/add', methods=["POST"])
 @tokenAdminRequire
@@ -68,12 +69,13 @@ def create(curr_user):
     if not validateDate(data['start_period']) and not validateDate(data['end_period']):
         return "Podane formaty dat nie są poprawne", 404
     
-    start_period = datetime.strptime(data['start_period'], '%Y-%m-%dT%H:%M:%SZ')
-    end_period = datetime.strptime(data['end_period'], '%Y-%m-%dT%H:%M:%SZ')
-    
+    start_period = datetime.strptime(data['start_period'] + "T00:00:00Z", '%Y-%m-%dT%H:%M:%SZ')
+    curr_end_period = datetime.strptime(data['end_period'] + "T00:00:00Z", '%Y-%m-%dT%H:%M:%SZ')
+    end_period = curr_end_period + timedelta(days=1)
+
     notPaidCases = NotPaidCase.query.filter(
         NotPaidCase.detect_time <= end_period, 
-        NotPaidCase.detect_time>= start_period
+        NotPaidCase.detect_time >= start_period
     ).all()
     
     problematicCases = ProblematicCase.query.filter(
@@ -81,16 +83,17 @@ def create(curr_user):
         ProblematicCase.detect_time <= end_period
     ).all()
 
-    filename = start_period.strftime("%Y%m%d")+"-"+end_period.strftime("%Y%m%d")
+    filename = datetime.strftime(start_period, "%Y%m%d") + "-" + datetime.strftime(end_period, "%Y%m%d") + "_" + getUuid()
     report = Report(start_period, end_period, filename, curr_user['id'])
+
+    pdfFilename = filename
+    generatePDF(pdfFilename, start_period, end_period, notPaidCases, problematicCases)
+
+    xlsxFilename = filename + '.xlsx'
+    generateXLSX(filename, notPaidCases, problematicCases)
+        
     db.session.add(report)
     db.session.commit()
 
-    pdfFilename = filename + '_' + getUuid() + '.pdf'
-    generatePDF(pdfFilename, start_period, end_period, notPaidCases, problematicCases)
-
-    xlsxFilename = filename + '_' + getUuid() + '.xlsx'
-    generateXLSX(filename, notPaidCases, problematicCases)
-        
     data = {"xslx_name": xlsxFilename, "pdf_name": pdfFilename}
     return makeResponse(data, 200)
