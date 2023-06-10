3faed158f748db098f1969073292502533ec86ec
Jakub Maciejewski <jakubm55555@gmail.com>
2023-06-10 16:31:08 +0200

    [168]: HOT FIX Add missing file

---
+++ MobileApp/app/src/main/java/pw/ee/proj_zesp/skp/track/TrackPoint.kt
@@ -0,0 +1,12 @@
+package pw.ee.proj_zesp.skp.track
+
+class TrackPoint(
+    var x: Double,
+    var y: Double,
+    var toLeft: Double,
+    var toRight: Double,
+    var toTop: Double,
+    var toBot: Double
+) {
+
+}
